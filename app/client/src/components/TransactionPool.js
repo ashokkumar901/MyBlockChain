897e10e5d00e811d47add79db185058df870abb5
Ashokkumar901 <ashokkumardonkada@gmail.com>
2020-04-11 17:32:57 +0530

    Transaction Pool component added

---
+++ app/client/src/components/TransactionPool.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Transaction from './Transaction';
+import { Link } from 'react-router-dom';
+
+class TransactionPool extends Component {
+    state = {
+        transactionPool: {}
+    };
+
+    fetchTransactionPoolMap = () => {
+        fetch('http://localhost:3001/transactions')
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    transactionPool: json
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.fetchTransactionPoolMap();
+    }
+
+    render() {
+        return (
+            <div className='TransactionPool'>
+                <div><Link to='/'>Home</Link></div>
+                <h3>Transaction Pool</h3>
+                {
+                    Object.values(this.state.transactionPool).map(transaction => {
+                        return (
+                            <div key={transaction.id}>
+                                <hr />
+                                <Transaction transaction={transaction} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default TransactionPool;
