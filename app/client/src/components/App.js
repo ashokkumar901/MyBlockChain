0468011a040a6e07d3e22b5459089bbe2888ee64
Ashokkumar901 <ashokkumardonkada@gmail.com>
2020-04-11 15:22:45 +0530

    react router added

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -23,13 +23,15 @@ class App extends Component {
                     Welcome to BlockChain!!!
                 </div>
                 <br />
+                <div>
+                    <Link to='/blocks'>Blocks</Link>
+                </div>
+                <br />
                 <div className='WalletInfo'>
                     <div>Address:{address}</div>
                     <div>Balance:{balance}</div>
                 </div>
 
-                <br />
-                <Blocks />
             </div >
         );
     }
